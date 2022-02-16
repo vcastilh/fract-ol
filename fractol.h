fea5ef6300256918a8ffa398154be149f46a3626
vcastilh <vcastilh@student.42sp.org.br>
2022-02-16 10:56:09 -0300

    feat: added ft_set_fractal to configure the sets
    Create structs to required set and to X11 api mlx

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: vcastilh <vcastilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 07:36:36 by vcastilh          #+#    #+#             */
-/*   Updated: 2022/02/16 07:40:05 by vcastilh         ###   ########.fr       */
+/*   Updated: 2022/02/16 10:30:20 by vcastilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,33 @@
  # define FRACTOL_H
  # include <unistd.h>
  # include "libft.h"
-void	ft_usage(void);
+
+typedef	struct	s_complex
+{
+	long double		re;
+	long double		im;
+	unsigned int	mdl;
+}	t_complex;
+
+typedef	struct	s_fractal
+{
+	char		*name;
+	t_complex	c;
+	t_complex	z;
+	t_complex	max;
+	t_complex	min;
+}	t_fractal;
+
+typedef	struct	s_mlx
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	void	*img;
+	char	*img_addr;
+}	t_mlx;
+
+void		ft_usage(void);
+void		ft_set_fractal(char **argv, t_fractal *fractal);
+t_complex	ft_complex(long double re, long double im);
 #endif
 
--- main.c
+++ main.c
@@ -6,19 +6,29 @@
 /*   By: vcastilh <vcastilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 07:24:26 by vcastilh          #+#    #+#             */
-/*   Updated: 2022/02/16 08:03:15 by vcastilh         ###   ########.fr       */
+/*   Updated: 2022/02/16 10:53:25 by vcastilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
-
+#include <stdio.h>
 int	main(int argc, char **argv)
 {
+	t_fractal	fractal;
+
 	if (argc < 2)
+	{
 		ft_usage();
+		exit(1);
+	}
 	else if (ft_strncmp(argv[1], "Mandelbrot", 11) != 0
 			&& (ft_strncmp(argv[1], "Julia", 6) == 0 && argc < 4))
+	{	
 		ft_usage();
+		exit(1);
+	}
+	ft_set_fractal(argv, &fractal);
+	printf("re: %Lf\t im: %Lf\t", fractal.c.re, fractal.c.im);
 	return (0);
 }
 
@@ -26,3 +36,29 @@ void	ft_usage(void)
 {
 	write(1, "Error\n", 6);
 }
+
+void	ft_set_fractal(char **argv, t_fractal *fractal)
+{
+	fractal->max = ft_complex(2, 2);
+	fractal->min = ft_complex(-2, -2);
+	if (argv[1][0] == 'M')
+	{
+		fractal->name = argv[1];
+		fractal->c = ft_complex(0, 0);
+	}
+	else if (argv[1][0] == 'J')
+	{
+		fractal->name = argv[1];
+		fractal->c = ft_complex(ft_atold(argv[2]),ft_atold(argv[3]));
+	}
+}
+
+t_complex	ft_complex(long double re, long double im)
+{
+	t_complex	numb;
+	
+	numb.re = re;
+	numb.im = im;
+	return (numb);
+}
+
