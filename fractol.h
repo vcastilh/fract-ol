aa877bffac03c6e846a0afb130f6c4690eb7cde8
vcastilh <vcastilh@student.42sp.org.br>
2022-02-18 13:58:08 -0300

    feat: added mandelbrot set

--- fractol.h
+++ fractol.h
@@ -6,14 +6,22 @@
 /*   By: vcastilh <vcastilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 07:36:36 by vcastilh          #+#    #+#             */
-/*   Updated: 2022/02/16 10:30:20 by vcastilh         ###   ########.fr       */
+/*   Updated: 2022/02/18 09:50:38 by vcastilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
  # define FRACTOL_H
+ # define WIDTH 500
+ # define HEIGHT 500
+ # define IMG_WIDTH 500
+ # define IMG_HEIGHT 500
+ # define MAX_ITER 2000
+ # define DIVERGENCE 3
  # include <unistd.h>
  # include "libft.h"
+ # include "mlx.h"
+ # include <math.h>
 
 typedef	struct	s_complex
 {
@@ -27,8 +35,11 @@ typedef	struct	s_fractal
 	char		*name;
 	t_complex	c;
 	t_complex	z;
+	t_complex	pow;
 	t_complex	max;
 	t_complex	min;
+	long double	s_x;
+	long double s_y;
 }	t_fractal;
 
 typedef	struct	s_mlx
@@ -36,11 +47,19 @@ typedef	struct	s_mlx
 	void	*mlx_ptr;
 	void	*win_ptr;
 	void	*img;
+	int		bpp;
+	int		endian;
+	int		line_size;
 	char	*img_addr;
 }	t_mlx;
 
 void		ft_usage(void);
 void		ft_set_fractal(char **argv, t_fractal *fractal);
 t_complex	ft_complex(long double re, long double im);
+void		ft_init_mlx(t_mlx *mlx, t_fractal *fractal);
+void		ft_mandelbrot(t_fractal *fractal, t_mlx *mlx);
+void		ft_julia(t_fractal *fractal, t_mlx *mlx);
+void		ft_win_to_view(t_fractal *fractal, int i, int j);
+int			ft_color(int t);
+void		ft_pixel_to_image(t_mlx *mlx, int i, int j, int clr);
 #endif
-
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vcastilh <vcastilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 07:24:26 by vcastilh          #+#    #+#             */
-/*   Updated: 2022/02/16 10:53:25 by vcastilh         ###   ########.fr       */
+/*   Updated: 2022/02/18 13:57:49 by vcastilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int	main(int argc, char **argv)
 {
 	t_fractal	fractal;
+	t_mlx		mlx;
 
 	if (argc < 2)
 	{
@@ -28,7 +29,14 @@ int	main(int argc, char **argv)
 		exit(1);
 	}
 	ft_set_fractal(argv, &fractal);
-	printf("re: %Lf\t im: %Lf\t", fractal.c.re, fractal.c.im);
+	ft_init_mlx(&mlx, &fractal);
+	if (argv[1][0] == 'M')
+		ft_mandelbrot(&fractal, &mlx);
+//	else if (argv[1][0] == 'J')
+//		ft_julia(&fractal, &mlx);
+	//printf("re: %Lf\t im: %Lf\t", fractal.c.re, fractal.c.im);
+	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr, mlx.img, 0, 0);
+	mlx_loop(mlx.mlx_ptr);
 	return (0);
 }
 
@@ -44,7 +52,6 @@ void	ft_set_fractal(char **argv, t_fractal *fractal)
 	if (argv[1][0] == 'M')
 	{
 		fractal->name = argv[1];
-		fractal->c = ft_complex(0, 0);
 	}
 	else if (argv[1][0] == 'J')
 	{
@@ -62,3 +69,78 @@ t_complex	ft_complex(long double re, long double im)
 	return (numb);
 }
 
+void	ft_init_mlx(t_mlx *mlx, t_fractal *fractal)
+{
+	mlx->mlx_ptr = mlx_init();
+	if (mlx->mlx_ptr == NULL)
+		exit(1);
+	mlx->win_ptr = mlx_new_window(mlx->mlx_ptr, WIDTH, HEIGHT, fractal->name);
+	if (mlx->win_ptr == NULL)
+	{
+		free(mlx->win_ptr);
+		exit(1);
+	}
+	mlx->img = mlx_new_image(mlx->mlx_ptr, IMG_WIDTH, IMG_HEIGHT);
+	mlx->img_addr = (char *)mlx_get_data_addr(mlx->img, &mlx->bpp, &mlx->line_size,
+			&mlx->endian);
+}
+
+void	ft_win_to_view(t_fractal *fractal, int i, int j)
+{
+	fractal->s_x = (fractal->max.re - fractal->min.re) / WIDTH;
+	fractal->s_y = (fractal->max.im - fractal->min.im) / HEIGHT;
+	fractal->c.re = fractal->min.re + (double)(j * fractal->s_x);
+	fractal->c.im = fractal->min.im + (double)(i * fractal->s_y);
+	fractal->z.re = 0;
+	fractal->z.im = 0;
+}
+
+void	ft_mandelbrot(t_fractal *fractal, t_mlx *mlx)
+{
+	int	i;
+	int	j;
+	int	iter;
+
+	i = 0;
+	while (i < HEIGHT)
+	{
+		j = 0;
+		while (j < WIDTH)
+		{
+			ft_win_to_view(fractal, i, j);
+			fractal->z.mdl = pow(fractal->c.re, 2) + pow(fractal->c.im, 2);
+			iter = 0;
+			while (fractal->z.mdl <= DIVERGENCE && iter < MAX_ITER)
+			{
+				fractal->z = ft_complex(pow(fractal->z.re,2) - pow(fractal->z.im, 2)
+						+ fractal->c.re, 2 * fractal->z.re * fractal->z.im
+						+ fractal->c.im);
+				fractal->z.mdl = pow(fractal->z.re, 2) + pow(fractal->z.im, 2);
+				iter++;
+			}
+			ft_pixel_to_image(mlx, i, j, ft_color(iter));
+			j++;
+		}
+		i++;
+	}
+}
+
+int	ft_color(int t)
+{
+	int	r;
+	int	g;
+	int b;
+
+	r = (int)(9 * (1 - t) * pow(t, 3) * 150);
+	g = (int)(15 * pow((1 - t), 2) * pow(t, 2) * 255);
+	b = (int)(8.5 * pow((1 - t), 3) * t * 80);
+	return ((int)(t << 24 | r << 16 | g << 8 | b));
+}
+
+void	ft_pixel_to_image(t_mlx *mlx, int i, int j, int clr)
+{
+	int	*p;
+
+	p = (int *)&mlx->img_addr[(i * mlx->line_size + (j * (mlx->bpp/8)))];
+	*p = clr;
+}
